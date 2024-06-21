6f57ec24a09228dd867c5b6b477e115dc992e397
lemus <lemus777@gmail.com>
2024-06-21 11:40:32 +0200

    migrating from custom Hook to reducer

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { cartReducer, initialState } from "./reducers/cart-reducer"
 
 function App() {
 
-  const { data, cart, addToCart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart, isEmpty, cartTotal } = useCart()
+  const { cart, addToCart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart, isEmpty, cartTotal } = useCart()
   const [state, dispatch] = useReducer(cartReducer, initialState)
 
   console.log(state)
@@ -27,7 +27,7 @@ function App() {
           <h2 className="text-center">Nuestra Colección</h2>
 
           <div className="row mt-5">
-              {data.map((guitar) => (
+              {state.data.map((guitar) => (
                   <Guitar 
                     key={guitar.id}
                     guitar={guitar}
